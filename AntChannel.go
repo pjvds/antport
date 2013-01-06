feff8c10c2ee8405a55056c18f416988b7e76192
Pieter Joost van de Sande <pj@born2code.net>
2013-01-06 19:45:18 +0100

    Introduce AntChannel ctor

--- AntChannel.go
+++ AntChannel.go
@@ -10,8 +10,25 @@ const (
 	ANT_PRODUCT_ID = 0x1008
 )
 
+type AntMessage struct {
+}
+
 type AntChannel struct {
-	device *usb.Device
+	device   *usb.Device
+	endpoint usb.Endpoint
+}
+
+func newAntChannel(usb *usb.Device) *AntChannel {
+	epoint, err := usb.OpenEndpoint(1, 0, 0, 1)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &AntChannel{
+		device:   usb,
+		endpoint: epoint,
+	}
 }
 
 func (channel *AntChannel) Close() {
@@ -19,4 +36,8 @@ func (channel *AntChannel) Close() {
 
 	channel.device.Close()
 	channel.device = nil
+}
+
+func (channel *AntChannel) ReceiveMessage() *AntMessage {
+	return nil
 }
--- AntContext.go
+++ AntContext.go
@@ -2,6 +2,7 @@ package antport
 
 import (
 	"github.com/kylelemons/gousb/usb"
+	"github.com/kylelemons/gousb/usbid"
 	"log"
 )
 
@@ -21,11 +22,35 @@ func NewContext() *AntContext {
 
 func (ctx *AntContext) ListChannels() ([]*AntChannel, error) {
 	devs, err := ctx.usb.ListDevices(func(desc *usb.Descriptor) bool {
+		log.Printf("Found %03d.%03d %s:%s %s\n", desc.Bus, desc.Address, desc.Vendor, desc.Product, usbid.Describe(desc))
+
 		// The usbid package can be used to print out human readable information.
-		log.Printf("%03d.%03d %s:%s\n", desc.Bus, desc.Address, desc.Vendor, desc.Product)
+		log.Printf("  Protocol: %s\n", usbid.Classify(desc))
 
 		// We are looking for the specific vendor and device
 		if desc.Vendor == ANT_VENDOR_ID && desc.Product == ANT_PRODUCT_ID {
+			log.Println("This is an ANT device")
+
+			// The configurations can be examined from the Descriptor, though they can only
+			// be set once the device is opened.  All configuration references must be closed,
+			// to free up the memory in libusb.
+			for _, cfg := range desc.Configs {
+				// This loop just uses more of the built-in and usbid pretty printing to list
+				// the USB devices.
+				log.Printf("  %s:\n", cfg)
+				for _, alt := range cfg.Interfaces {
+					log.Printf("    --------------\n")
+					for _, iface := range alt.Setups {
+						log.Printf("    %s\n", iface)
+						log.Printf("      %s\n", usbid.Classify(iface))
+						for _, end := range iface.Endpoints {
+							log.Printf("      %s\n", end)
+						}
+					}
+				}
+				log.Printf("    --------------\n")
+			}
+
 			return true
 		}
 
@@ -42,17 +67,14 @@ func (ctx *AntContext) ListChannels() ([]*AntChannel, error) {
 	channels := make([]*AntChannel, nDevices)
 	for i := 0; i < len(devs); i++ {
 		usbDevice := devs[i]
-		chnl := &AntChannel{
-			device: usbDevice,
-		}
 
-		channels[i] = chnl
+		channels[i] = newAntChannel(usbDevice)
 	}
 	return channels, nil
 }
 
 func (ctx *AntContext) Close() {
-	//log.Print("closing usb context") <-- CAUSES ERROR :-S
+	// log.Print("closing usb context") // TODO <-- CAUSES ERROR :-S
 
 	ctx.usb.Close()
 	ctx.usb = nil
