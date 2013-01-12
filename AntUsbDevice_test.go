4a10234eccde0a544ccf31bf910126f0393a6c8a
Pieter Joost van de Sande <pj@born2code.net>
2013-01-12 13:21:42 +0100

    Fix GetSingleChannelOrFail to use given message

--- AntUsbDevice_test.go
+++ AntUsbDevice_test.go
@@ -12,4 +12,35 @@ func TestOpenContext(t *testing.T) {
 	resetCommand := CreateResetCommand()
 	device.Write(resetCommand.Pack())
 	device.Read(make([]byte, 8))
+
+	capCommand := CreateCapabilitiesCommand()
+	device.Write(capCommand.Pack())
+	device.Read(make([]byte, 8))
+}
+
+func GetSingleChannelOrFail(t *testing.T) (*AntUsbDevice, *AntUsbContext) {
+	ctx := NewUsbContext()
+	channels, err := ctx.ListAntUsbDevices()
+
+	if err != nil {
+		CloseContextAndFail(t, ctx, "ListChannels failed with error: %v")
+
+	}
+
+	if len(channels) == 0 {
+		CloseContextAndFail(t, ctx, "no channels available")
+	}
+
+	if len(channels) > 1 {
+		CloseContextAndFail(t, ctx, "multiple channels available")
+	}
+
+	channel := channels[0]
+	return channel, ctx
+}
+
+func CloseContextAndFail(t *testing.T, ctx *AntUsbContext, message string) {
+	defer ctx.Close()
+	t.Error(message)
+	t.FailNow()
 }
